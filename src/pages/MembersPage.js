a3dbb4cd0ea6eeeabdf35f96873cb2bce30bc1d5
Rabiya-Abubakar <Rabiya.abubakar1@student.moringaschool.com>
2024-09-16 20:17:30 +0300

    Update MembersPage.js

--- src/pages/MembersPage.js
+++ src/pages/MembersPage.js
@@ -9,7 +9,7 @@ const MembersPage = () => {
 
     useEffect(() => {
         // Fetch all members from the JSON server
-        fetch(`${process.env.REACT_APP_API_URL}`) // Using backticks for template literal
+        fetch('http://localhost:5000/members')
             .then((response) => response.json())
             .then((data) => setMembers(data))
             .catch((error) => console.error('Error fetching members:', error));
@@ -23,7 +23,7 @@ const MembersPage = () => {
             ) : (
                 <p>Please log in to see member content.</p>
             )}
-
+            
             <div className="members-grid">
                 {members.length > 0 ? (
                     members.map((member) => (
